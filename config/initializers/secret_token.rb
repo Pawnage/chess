fd6a78c5ee397e6c9d9fcf826ce28068aeb0a8b5
Cynrick Chiu <cyn.2k5@outlook.com>
2015-06-08 22:29:02 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chess::Application.config.secret_key_base = '6bb80183fa1b9462e9cfa9880cc28266abb841582f4202a9330c0910b3a517d852b2ffd497c0bb2b37a491bf554514b311bc3390a0e6e9942c822cb0bbf8c040'
