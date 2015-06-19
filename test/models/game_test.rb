4a37b5136e5c048b16ac370ad1f63feb2cb7d40c
Cynrick Chiu <cyn.2k5@outlook.com>
2015-06-19 23:15:56 +0000

    Fix typo, add dashboards#show and corresponding views page

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -3,9 +3,8 @@ require 'test_helper'
 class GameTest < ActiveSupport::TestCase
 
   test "create_game" do
-     game = Game.create(:player_white => 1, :player_black => 2, :player_turn => 1, :winner => 1, :moves => 1)
-  	 puts game.inspect
+    game = Game.create(:player_white => 1, :player_black => 2, :player_turn => 1, :winner => 1, :moves => 1)
+  	puts game.inspect
   end
-
 
 end
