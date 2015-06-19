4f5e5aadb5f3f7055b395193949444009b1adac6
MelanieJae <mtaurus575@gmail.com>
2015-06-19 23:42:30 +0000

    Resolve merge conflict in piece model re: updated attribute names

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -4,7 +4,6 @@ class Piece < ActiveRecord::Base
 	attr_accessor :row_position, :col_position, :color, :type, :alive
 
 	def initialize(row, col, color, type, alive=true)
->>>>>>> 4a37b5136e5c048b16ac370ad1f63feb2cb7d40c
 		self.row_position = row
 		self.col_position = col
 		self.color = color
