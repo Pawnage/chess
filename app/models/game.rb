85cb872b4c08efae1779464f3461215ae33a06f1
Cynrick Chiu <cyn.2k5@outlook.com>
2015-06-20 09:10:26 +0000

    Edit models/game.rb and test/models/game_test.rb

--- app/models/game.rb
+++ app/models/game.rb
@@ -10,68 +10,42 @@ class Game < ActiveRecord::Base
 	# end
 
 	def populate_board!
-		# create black pieces
-		Piece.create(:game_id => id, :row_position => 8, :col_position => 1, :color => "black", :type => "Rook", :alive => true)
-		Piece.create(:game_id => id, :row_position => 8, :col_position => 2, :color => "black", :type => "Knight", :alive => true)
-		Piece.create(:game_id => id, :row_position => 8, :col_position => 3, :color => "black", :type => "Bishop", :alive => true)
-		Piece.create(:game_id => id, :row_position => 8, :col_position => 4, :color => "black", :type => "Queen", :alive => true)
-		Piece.create(:game_id => id, :row_position => 8, :col_position => 5, :color => "black", :type => "King", :alive => true)
-		Piece.create(:game_id => id, :row_position => 8, :col_position => 6, :color => "black", :type => "Bishop", :alive => true)
-		Piece.create(:game_id => id, :row_position => 8, :col_position => 7, :color => "black", :type => "Knight", :alive => true)
-		Piece.create(:game_id => id, :row_position => 8, :col_position => 8, :color => "black", :type => "Rook", :alive => true)
-
-		(1..8).each do |col|
-      Piece.create(:game_id => id, :row_position => 7, :col_position => col, :color => "black", :type => "Pawn", :alive => true)
-		end
-
-    # create white pieces
-    (1..8).each do |col|
-      Piece.create(:game_id => id, :row_position => 2, :col_position => col, :color => "white", :type => "Pawn", :alive => true)
-		end
-
-    Piece.create(:game_id => id, :row_position => 1, :col_position => 1, :color => "white", :type => "Rook", :alive => true)
-		Piece.create(:game_id => id, :row_position => 1, :col_position => 2, :color => "white", :type => "Knight", :alive => true)
-		Piece.create(:game_id => id, :row_position => 1, :col_position => 3, :color => "white", :type => "Bishop", :alive => true)
-		Piece.create(:game_id => id, :row_position => 1, :col_position => 4, :color => "white", :type => "Queen", :alive => true)
-		Piece.create(:game_id => id, :row_position => 1, :col_position => 5, :color => "white", :type => "King", :alive => true)
-		Piece.create(:game_id => id, :row_position => 1, :col_position => 6, :color => "white", :type => "Bishop", :alive => true)
-		Piece.create(:game_id => id, :row_position => 1, :col_position => 7, :color => "white", :type => "Knight", :alive => true)
-		Piece.create(:game_id => id, :row_position => 1, :col_position => 8, :color => "white", :type => "Rook", :alive => true)
+    @set = []
 
 		# populate front rows
-		# (0..7).each do |n|
-		# 	@set << Piece.new(Pawn, white, 1, n)
-		# 	@set << Piece.new(Pawn, black, 6, n)
-		# end
+		(0..7).each do |n|
+			@set << Piece.create(:game_id => id, :type => "Pawn", :color => "white", :row_position => 1, :col_position => n, :alive => true)
+			@set << Piece.create(:game_id => id, :type => "Pawn", :color => "black", :row_position => 6, :col_position => n, :alive => true)
+		end
 
 		# populate back rows
 		# rook,knight,bishop,queen,king,bishop,knight,rook
-		# rooks		
-		# @set << Piece.new(Rook, white, 0, 0)
-		# @set << Piece.new(Rook, white, 0, 7)
-		# @set << Piece.new(Rook, black, 7, 0)
-		# @set << Piece.new(Rook, black, 7, 7)
+		# rooks
+		@set << Piece.create(:game_id => id, :type => "Rook", :color => "white", :row_position => 0, :col_position => 0, :alive => true)
+		@set << Piece.create(:game_id => id, :type => "Rook", :color => "white", :row_position => 0, :col_position => 7, :alive => true)
+		@set << Piece.create(:game_id => id, :type => "Rook", :color => "black", :row_position => 7, :col_position => 0, :alive => true)
+		@set << Piece.create(:game_id => id, :type => "Rook", :color => "black", :row_position => 7, :col_position => 7, :alive => true)
 		
 		# knights	
-		# @set << Piece.new(Knight, white, 0, 2)		
-		# @set << Piece.new(Knight, white, 0, 6)
-		# @set << Piece.new(Knight, black, 7, 2)
-		# @set << Piece.new(Knight, black, 7, 6)
+		@set << Piece.create(:game_id => id, :type => "Knight", :color => "white", :row_position => 0, :col_position => 2, :alive => true)		
+		@set << Piece.create(:game_id => id, :type => "Knight", :color => "white", :row_position => 0, :col_position => 6, :alive => true)
+		@set << Piece.create(:game_id => id, :type => "Knight", :color => "black", :row_position => 7, :col_position => 2, :alive => true)
+		@set << Piece.create(:game_id => id, :type => "Knight", :color => "black", :row_position => 7, :col_position => 6, :alive => true)
 		
 		# bishops		
-		# @set << Piece.new(Bishop, white, 0, 3)
-		# @set << Piece.new(Bishop, white, 0, 5)	
-		# @set << Piece.new(Bishop, black, 7, 3)
-		# @set << Piece.new(Bishop, black, 7, 5)
+		@set << Piece.create(:game_id => id, :type => "Bishop", :color => "white", :row_position => 0, :col_position => 3, :alive => true)
+		@set << Piece.create(:game_id => id, :type => "Bishop", :color => "white", :row_position => 0, :col_position => 5, :alive => true)	
+		@set << Piece.create(:game_id => id, :type => "Bishop", :color => "black", :row_position => 7, :col_position => 3, :alive => true)
+		@set << Piece.create(:game_id => id, :type => "Bishop", :color => "black", :row_position => 7, :col_position => 5, :alive => true)
 
 		# queens
-		# @set << Piece.new(Queen, white, 0, 3)
-		# @set << Piece.new(Queen, white, 7, 3)
+		@set << Piece.create(:game_id => id, :type => "Queen", :color => "white", :row_position => 0, :col_position => 3, :alive => true)
+		@set << Piece.create(:game_id => id, :type => "Queen", :color => "white", :row_position => 7, :col_position => 3, :alive => true)
 		
 		# kings
-		# @set << Piece.new(King, black, 0, 4)
-		# @set << Piece.new(King, black, 7, 4)	
+		@set << Piece.create(:game_id => id, :type => "King", :color => "black", :row_position => 0, :col_position => 4, :alive => true)
+		@set << Piece.create(:game_id => id, :type => "King", :color => "black", :row_position => 7, :col_position => 4, :alive => true)	
 
-		# return @set
+		return @set
 	end
 end
--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -3,12 +3,6 @@ require 'test_helper'
 class GameTest < ActiveSupport::TestCase
   test "#populate_board!" do
     game = FactoryGirl.create(:game)
-  	assert_equal 32, game.pieces.count
-  	assert_equal 16, Pawn.all.count
-  	assert_equal 4, Rook.all.count
-  	assert_equal 4, Knight.all.count
-  	assert_equal 4, Bishop.all.count
-  	assert_equal 2, Queen.all.count
-  	assert_equal 2, King.all.count
+    assert_equal 32, game.pieces.count
   end
 end
