4bbbc65a30463ad811e3fe59eba717ee1324f43f
MelanieJae <mtaurus575@gmail.com>
2015-06-22 18:44:51 +0000

    attempt to clear unknown piece method error

--- app/models/game.rb
+++ app/models/game.rb
@@ -9,39 +9,39 @@ class Game < ActiveRecord::Base
 	def populate_board!		
 		#populate front rows wth pawns
 		(0..7).each do |n|
-			pieces.create(:type => Pawn, :color => :player_white, :row_position => 1, :col_position => n)
+			pieces.create(:type => 'Pawn', :color => :player_white, :row_position => 1, :col_position => n)
 			
 		end
 
 	# 	#populate back rows
 	# 	#rook,knight,bishop,queen,king,bishop,knight,rook
 	# 	#rooks		
-		pieces.create(:type => Rook, :color => :player_white, :row_position => 0, :col_position => 0)
-		pieces.create(:type => Rook, :color => :player_white, :row_position => 0, :col_position => 7)
-		pieces.create(:type => Rook, :color => :player_white, :row_position => 7, :col_position => 0)
-		pieces.create(:type => Rook, :color => :player_white, :row_position => 7, :col_position => 7)
+		pieces.create(:type => 'Rook', :color => :player_white, :row_position => 0, :col_position => 0)
+		pieces.create(:type => 'Rook', :color => :player_white, :row_position => 0, :col_position => 7)
+		pieces.create(:type => 'Rook', :color => :player_white, :row_position => 7, :col_position => 0)
+		pieces.create(:type => 'Rook', :color => :player_white, :row_position => 7, :col_position => 7)
 	
 		
 	# 	#knights
-		pieces.create(:type => Knight, :color => :player_white, :row_position => 0, :col_position => 2)	
-		pieces.create(:type => Knight, :color => :player_white, :row_position => 0, :col_position => 6)	
-		pieces.create(:type => Knight, :color => :player_black, :row_position => 7, :col_position => 2)	
-		pieces.create(:type => Knight, :color => :player_black, :row_position => 7, :col_position => 6)	
+		pieces.create(:type => 'Knight', :color => :player_white, :row_position => 0, :col_position => 2)	
+		pieces.create(:type => 'Knight', :color => :player_white, :row_position => 0, :col_position => 6)	
+		pieces.create(:type => 'Knight', :color => :player_black, :row_position => 7, :col_position => 2)	
+		pieces.create(:type => 'Knight', :color => :player_black, :row_position => 7, :col_position => 6)	
 
 		
 	# 	#bishops
-		pieces.create(:type => Bishop, :color => :player_white, :row_position => 0, :col_position => 3)
-		pieces.create(:type => Bishop, :color => :player_white, :row_position => 0, :col_position => 5)
-		pieces.create(:type => Bishop, :color => :player_black, :row_position => 7, :col_position => 3)
-		pieces.create(:type => Bishop, :color => :player_black, :row_position => 7, :col_position => 5)
+		pieces.create(:type => 'Bishop', :color => :player_white, :row_position => 0, :col_position => 3)
+		pieces.create(:type => 'Bishop', :color => :player_white, :row_position => 0, :col_position => 5)
+		pieces.create(:type => 'Bishop', :color => :player_black, :row_position => 7, :col_position => 3)
+		pieces.create(:type => 'Bishop', :color => :player_black, :row_position => 7, :col_position => 5)
 
 	# 	#queens
-		pieces.create(:type => Queen, :color => :player_white, :row_position => 0, :col_position => 3)
-		pieces.create(:type => Queen, :color => :player_black, :row_position => 7, :col_position => 3)
+		pieces.create(:type => 'Queen', :color => :player_white, :row_position => 0, :col_position => 3)
+		pieces.create(:type => 'Queen', :color => :player_black, :row_position => 7, :col_position => 3)
 	
 	#kings
-		pieces.create(:type => King, :color => :player_white, :row_position => 0, :col_position => 4)
-		pieces.create(:type => King, :color => :player_black, :row_position => 7, :col_position => 4)
+		pieces.create(:type => 'King', :color => :player_white, :row_position => 0, :col_position => 4)
+		pieces.create(:type => 'King', :color => :player_black, :row_position => 7, :col_position => 4)
 	
 
 	end
