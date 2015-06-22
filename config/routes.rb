b39ad4109383496ea16c42db9c9c8a02f51ce581
Noah Finberg <noahfinberg@gmail.com>
2015-06-22 15:27:18 +0000

    set up routes and controller (initially) for game CRUD--still need to hook up to views and make controller work

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Chess::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static_pages#index'
 
+  resources :games, except: [:index]
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
