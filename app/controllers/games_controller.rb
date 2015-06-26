1f47c761662faa7e7618594e6f78105565821816
Noah Finberg <noahfinberg@gmail.com>
2015-06-26 22:13:09 +0000

    fixed games controller to account for field name changes

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,13 +1,17 @@
 class GamesController < ApplicationController
   before_action :authenticate_player!, :only => [:new, :create]
 
-  def index
-  	@games = Game.all
-  end
+  	def index
+  		@games = Game.all
+  	end
+
+  	def new
+  		@game = Game.new
+  	end
 
 
 	def create
-		@game = Game.create(:player_white => current_player.id, :player_black => nil, :player_turn => current_player.id, :winner => nil, :moves => 0)
+		@game = Game.create(:player_white_id => current_player.id, :player_black_id => nil, :player_turn => current_player.id, :winner => nil, :moves => 0)
 		redirect_to game_path(@game)
 	end
 
@@ -18,6 +22,6 @@ class GamesController < ApplicationController
 	private
 
 	def game_params
-		params.require(:game).permit(:player_white, :player_black, :player_turn, :winner, :moves)	  
+		params.require(:game).permit(:player_white_id, :player_black_id, :player_turn, :winner, :moves)	  
 	end
 end
