7203524a2c533039a1927d401b50f53826bf0d1b
Cynrick Chiu <cyn.2k5@outlook.com>
2015-07-03 00:35:24 +0000

    Add capture? method

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -6,7 +6,7 @@ class Piece < ActiveRecord::Base
 
   attr_accessor :selected
 	
-	def obstructed_move?(x, y)
+  def obstructed_move?(x, y)
     obstruction_array = obstructed_positions(x, y)
  
       #maybe return false if error
@@ -15,7 +15,7 @@ class Piece < ActiveRecord::Base
         # return true if obstruction found
         return true if game.obstruction(position[x], position[y])
       end
-	end
+  end
 
   # for highlighting piece when selected
   def selected!
@@ -30,6 +30,10 @@ class Piece < ActiveRecord::Base
 
 	def legal_move?
 	end
+
+  def capture?(x, y)
+    !Piece.where(game_id: game.id, row_position: x, col_position: y).not(color: color).empty?
+  end
 
   def set_default_for_alive
     self.alive = true if self.alive.nil?
