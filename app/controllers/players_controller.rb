e61a5805aad50fe89cc47ddc0c826b3abba9e3b1
MelanieJae <mtaurus575@gmail.com>
2015-07-03 21:48:44 +0000

    need to fix 2nd, 3rd links so they work outside games loop

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -5,11 +5,5 @@ class PlayersController < ApplicationController
 
   private
   
-	# a start to connecting the player_id to a color
-	# and game since player id is attached to a color in a game
-	# but it could be a different color in each game
-	# def player_params
-	# 	params.require(:player).permit(...)
-	# end
-
+	
 end
