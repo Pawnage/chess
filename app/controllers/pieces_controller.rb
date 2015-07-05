31bbc173ee2c5f6657932bc58fd04cfa85e4fbf1
MelanieJae <mtaurus575@gmail.com>
2015-07-05 21:39:20 +0000

    legal move edits

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -14,16 +14,8 @@ class PiecesController < ApplicationController
     row = params[:row_position]
     col = params[:col_position]
     @piece.update_attributes(:row_position => row, :col_position => col)
-      if @piece.type == 'Pawn' 
-        #add line that detects when the attribute update results in a two-square move by the pawn
-        
-        #calls method that overrides the standard legal move method and sets legal_move? = true so
-        # that legal_move can be called on the pawn for all future moves as with the other pieces. 
-        @piece.game.move_two_squares?
-        if piece.update_attributes(:row_position => row, :col_position => col).legal_move?
+      if piece.update_attributes(:row_position => row, :col_position => col).legal_move?
         redirect_to game_path(@piece.game)
-      #for when legal_move? returns false, player must retract move and make a new move  
-      else @piece.update_attributes(:row_position => :col_position => col)
       end
   end
 
