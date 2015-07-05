fb6a2d1615f9f2ed5cc54162b3f602a97b24befa
Cynrick Chiu <cyn.2k5@outlook.com>
2015-07-05 18:37:49 +0000

    Add test for capture! method

--- test/models/piece_test.rb
+++ test/models/piece_test.rb
@@ -19,4 +19,16 @@ class PieceTest < ActiveSupport::TestCase
     black_pawn = FactoryGirl.create(:piece, :game_id => game.id, :type => 'Pawn', :color => 'Black', :row_position => 2, :col_position => 1)
     assert_equal true, white_pawn.capturable?(black_pawn.row_position, black_pawn.col_position)
   end
+
+  test "#capture! changes row_position, col_position, alive fields to nil, nil, false" do
+    game = FactoryGirl.create(:game)
+    white_pawn = FactoryGirl.create(:piece, :id => 1, :game_id => game.id, :type => 'Pawn', :color => 'White', :row_position => 1, :col_position => 1)
+    black_pawn = FactoryGirl.create(:piece, :id => 2, :game_id => game.id, :type => 'Pawn', :color => 'Black', :row_position => 2, :col_position => 1)
+    white_pawn.capture!(black_pawn.row_position, black_pawn.col_position)
+
+    updated_black_pawn = Piece.find(2)
+    assert_equal nil, updated_black_pawn.row_position
+    assert_equal nil, updated_black_pawn.col_position
+    assert_equal false, updated_black_pawn.alive
+  end
 end
