81052288a36c0757fe1f8622272fc238b3b018e5
Cynrick Chiu <cyn.2k5@outlook.com>
2015-07-06 19:12:20 +0000

    Edit model test for rook

--- test/models/rook_test.rb
+++ test/models/rook_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class RookTest < ActiveSupport::TestCase
 
   test "white rook_legal_move" do
-    rook = FactoryGirl.create(:rook, :type => 'Rook')
-    assert true, rook.rook_legal_move?(0, 2)
+    rook = FactoryGirl.create(:rook, :type => 'Rook', :color => 'White', :row_position => 0, :col_position => 0)
+    assert_equal true, rook.rook_legal_move?(0, 2)
   end
 end
