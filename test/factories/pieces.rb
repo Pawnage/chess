8745da428b8736569bcb8e5a8da81a142a82a46b
Cynrick Chiu <cyn.2k5@outlook.com>
2015-07-06 22:40:54 +0000

    Update pieces factory

--- test/factories/pieces.rb
+++ test/factories/pieces.rb
@@ -1,15 +1,16 @@
 FactoryGirl.define do
   factory :piece do
-  	row_position 1
-  	col_position 1
-  	color "White"
-  	type "Rook"
+#  	row_position 1
+#  	col_position 1
+#  	color "White"
+#  	type "Rook"
   	alive true
   	association :player
     association :game
   end
 
   factory :pawn, parent: :piece, class: 'Pawn' do
+    type "Pawn"
   end
 
   factory :rook, parent: :piece, class: 'Rook' do
