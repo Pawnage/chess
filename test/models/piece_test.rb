5b0987affb42a46515ba8fcc701257f9557ac05f
Cynrick Chiu <cyn.2k5@outlook.com>
2015-07-06 21:21:46 +0000

    Fix typo in test description for capturable? method

--- test/models/piece_test.rb
+++ test/models/piece_test.rb
@@ -6,7 +6,7 @@ class PieceTest < ActiveSupport::TestCase
     assert_equal false, white_pawn.capturable?(2, 1)
   end
 
-  test "#capture? returns false if specified coordinates occupied by piece of same color" do
+  test "#capturable? returns false if specified coordinates occupied by piece of same color" do
     game = FactoryGirl.create(:game)
     white_pawn_1 = FactoryGirl.create(:piece, :game_id => game.id, :type => 'Pawn', :row_position => 1, :col_position => 1)
     white_pawn_2 = FactoryGirl.create(:piece, :game_id => game.id, :type => 'Pawn', :row_position => 2, :col_position => 1)
