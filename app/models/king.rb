36fb1e6162368372f42cb02f063dd12321ccec52
MelanieJae <mtaurus575@gmail.com>
2015-07-08 01:31:33 +0000

    renamed king_legal_move, added condition to keep king move inside board

--- app/models/king.rb
+++ app/models/king.rb
@@ -6,12 +6,16 @@ class King < Piece
 	end
 
 
-	def legal_move?(x, y)	
-		if (self.row_position - x).abs == 1 or (self.col_position - y).abs == 1
-			return true		
+	def legal_move?(x, y)
+		if x >= 0 and x < 7 and y>= 0 and y < 7	
+			if (self.row_position - x).abs == 1 or (self.col_position - y).abs == 1
+				return true		
+			else	
+				return false
+			end
+			
 		else
 			return false
-		end	
-
+		end
 	end
 end
