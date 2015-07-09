ec038d1a5b78a053e0c33a0417cd6f4fa3c4034f
MelanieJae <mtaurus575@gmail.com>
2015-07-09 01:43:02 +0000

    created two basic tests for regular and off board bishop moves

--- test/models/bishop_test.rb
+++ test/models/bishop_test.rb
@@ -4,15 +4,10 @@ class BishopTest< ActiveSupport::TestCase
   test "legal_move forward returns true" do
     bishop = FactoryGirl.create(:bishop, :row_position => 2, :col_position => 0)
     assert true, bishop.legal_move?(1, 1)
-    assert true, bishop.legal_move?(6, 1)
-    assert true, bishop.legal_move?(1, 6)
-    assert true, bishop.legal_move?(6, 6)
   end
 
   test "legal_move off board returns false" do
-    refute bishop.legal_move?(1, -1)
-    refute bishop.legal_move?(6, -1)
-    refute bishop.legal_move?(1, 8)
-    refute bishop.legal_move?(6, 8)
+    bishop = FactoryGirl.create(:bishop, :row_position => 2, :col_position => 0)
+    refute bishop.legal_move?(3, -2)
   end
 end
