af4ca7b3d1ae9b79b64264b6d4273239aa9e1e8a
MelanieJae <mtaurus575@gmail.com>
2015-07-10 22:56:57 +0000

    added illegal on-board move tests

--- test/models/bishop_test.rb
+++ test/models/bishop_test.rb
@@ -9,6 +9,14 @@ class BishopTest< ActiveSupport::TestCase
     assert true, bishop.legal_move?(3, 1)
   end
 
+  test "legal_move move in one direction returns false" do
+    bishop = FactoryGirl.create(:bishop, :row_position => 4, :col_position => 2)
+    assert true, bishop.legal_move?(5, 3)
+    assert true, bishop.legal_move?(3, 3)
+    assert true, bishop.legal_move?(5, 1)
+    assert true, bishop.legal_move?(3, 1)
+  end
+
   test "legal_move off board returns false" do
     bishop = FactoryGirl.create(:bishop, :row_position => 2, :col_position => 0)
     refute bishop.legal_move?(3, -1)
