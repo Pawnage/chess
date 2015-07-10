c23ea468abb590e865350498f207173b795b06a4
MelanieJae <mtaurus575@gmail.com>
2015-07-10 22:59:10 +0000

    added illegal on-board move tests

--- test/models/rook_test.rb
+++ test/models/rook_test.rb
@@ -10,6 +10,13 @@ class RookTest < ActiveSupport::TestCase
     refute rook.legal_move?(8, 0)
   end
 
+  test "rook legal_move one move in two directions returns false" do
+  	rook = FactoryGirl.create(:rook, :row_position => 0, :col_position => 0)
+    refute rook.legal_move?(3, 1)
+    refute rook.legal_move?(5, 5)
+    refute rook.legal_move?(1, 1)
+  end
+
   #add tests for castling once castling logic is done
 
 end
