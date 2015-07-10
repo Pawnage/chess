761edd5120b15ed006a0a1e92c7fedd73c27665a
MelanieJae <mtaurus575@gmail.com>
2015-07-10 18:14:57 +0000

    added to bishop tests

--- test/models/bishop_test.rb
+++ test/models/bishop_test.rb
@@ -1,13 +1,19 @@
 require 'test_helper'
 
 class BishopTest< ActiveSupport::TestCase
-  test "legal_move forward returns true" do
-    bishop = FactoryGirl.create(:bishop, :row_position => 2, :col_position => 0)
-    assert true, bishop.legal_move?(1, 1)
+  test "legal_move diag returns true" do
+    bishop = FactoryGirl.create(:bishop, :row_position => 4, :col_position => 2)
+    assert true, bishop.legal_move?(5, 3)
+    assert true, bishop.legal_move?(3, 3)
+    assert true, bishop.legal_move?(5, 1)
+    assert true, bishop.legal_move?(3, 1)
   end
 
   test "legal_move off board returns false" do
     bishop = FactoryGirl.create(:bishop, :row_position => 2, :col_position => 0)
-    refute bishop.legal_move?(3, -2)
+    refute bishop.legal_move?(3, -1)
+    refute bishop.legal_move?(1, -1)
+    refute bishop.legal_move?(8, 6)
+    refute bishop.legal_move?(-1, 3)
   end
 end
