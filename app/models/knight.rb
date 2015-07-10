1018db4b353d968f6770118d5a1ad9291f60209c
MelanieJae <mtaurus575@gmail.com>
2015-07-10 19:14:16 +0000

    knight legal move logic and tests

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,12 +1,11 @@
 class Knight < Piece
   def legal_move?(x, y)
     # knight moves two squares in horiz. or vertical then one space in the other of the two directions 
-    if ((self.row_position - x).abs == 2 and (self.col_position - y).abs == 1) or ((self.col_position - y).abs == 2 and (self.row_position - x).abs == 1)
-      return true		
-    else
-      return false
-    end	
-  end
+    if x >= 0 and x <= 7 and y >= 0 and y <= 7
+      return true if ((self.row_position - x).abs == 2 and (self.col_position - y).abs == 1) or ((self.col_position - y).abs == 2 and (self.row_position - x).abs == 1)		
+    end
+    return false	
+  end 
 
   def obstructed_positions(x, y)
     # has no obstructed positions
