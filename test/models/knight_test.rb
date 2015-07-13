6b2a6854015aad245394a4e46a4ab1bf352549ee
MelanieJae <mtaurus575@gmail.com>
2015-07-13 21:40:38 +0000

    edits

--- test/models/knight_test.rb
+++ test/models/knight_test.rb
@@ -4,6 +4,6 @@ class KnightTest < ActiveSupport::TestCase
  test "white knight_legal_move" do
 
    knight = FactoryGirl.create(:knight, :type => 'Knight', :row_position => 1, :col_position => 0)
-   assert_equal true, knight.legal_move?(2, 2) 
+   assert knight.legal_move?(2, 2) 
   end
 end
