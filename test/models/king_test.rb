d0ae1ca3e00bccbdd5cab7e745b37ce57feb859a
MelanieJae <mtaurus575@gmail.com>
2015-07-15 12:43:33 +0000

    branch cleanup-legal move king

--- test/models/king_test.rb
+++ test/models/king_test.rb
@@ -2,16 +2,16 @@ require 'test_helper'
 
 class KingTest < ActiveSupport::TestCase
 
-  test "king legal_move? backward off board top returns false" do
-    king = FactoryGirl.create(:king, :row_position => 7, :col_position => 7)
+  test "king legal_move? off board returns false" do
+    king = FactoryGirl.create(:king, :row_position => 4, :col_position => 7)
     refute king.legal_move?(8, 8)
     refute king.legal_move?(-1, 7)
   end
 
   test "king legal_move? 1 square to the left or right returns true" do
-    king = FactoryGirl.create(:king, :row_position => 4, :col_position => 7)
-    assert king.legal_move?(3, 7)
-    assert king.legal_move?(5, 7)
+    king = FactoryGirl.create(:king, :row_position => 4, :col_position => 5)
+    assert king.legal_move?(3, 5)
+    assert king.legal_move?(5, 5)
   end
 
   test "king legal_move? 1 square diagonal downward left or upward right returns true" do
