448d028ca3c635949f6813df3b3dbff26bdf3eb5
MelanieJae <mtaurus575@gmail.com>
2015-07-15 12:37:40 +0000

    branch cleanup-legale move king

--- test/models/king_test.rb
+++ test/models/king_test.rb
@@ -5,41 +5,25 @@ class KingTest < ActiveSupport::TestCase
   test "king legal_move? backward off board top returns false" do
     king = FactoryGirl.create(:king, :row_position => 7, :col_position => 7)
     refute king.legal_move?(8, 8)
-  end
-
-  test "king legal_move? backward off board bottom returns false" do
-    king = FactoryGirl.create(:king, :row_position => 0, :col_position => 7)
     refute king.legal_move?(-1, 7)
   end
 
-  test "king legal_move? 1 square to the left returns true" do
-    king = FactoryGirl.create(:king, :row_position => 4, :col_position => 7)
-    assert king.legal_move?(4, 6)
-  end
-
-  test "king legal_move? 1 square to the right returns true" do
-    king = FactoryGirl.create(:king, :row_position => 4, :col_position => 6)
-    assert king.legal_move?(4, 7)
-  end
-
-  test "king legal_move? 1 square diagonal downward left returns true" do
-    king = FactoryGirl.create(:king, :row_position => 4, :col_position => 7)
-    assert king.legal_move?(3, 6)
-  end
-
-  test "king legal_move? 1 square diagonal upward right returns true" do
-    king = FactoryGirl.create(:king, :row_position => 4, :col_position => 6)
-    assert king.legal_move?(5, 7)
-  end
-
-  test "king legal_move? 1 square upward returns true" do
-    king = FactoryGirl.create(:king, :row_position => 4, :col_position => 7)
-    assert king.legal_move?(5, 7)
-  end
-
-  test "king legal_move? 1 square downward returns true" do
+  test "king legal_move? 1 square to the left or right returns true" do
     king = FactoryGirl.create(:king, :row_position => 4, :col_position => 7)
     assert king.legal_move?(3, 7)
+    assert king.legal_move?(5, 7)
+  end
+
+  test "king legal_move? 1 square diagonal downward left or upward right returns true" do
+    king = FactoryGirl.create(:king, :row_position => 4, :col_position => 5)
+    assert king.legal_move?(3, 6)
+    assert king.legal_move?(5, 5)
+  end
+
+  test "king legal_move? 1 square upward or downward returns true" do
+    king = FactoryGirl.create(:king, :row_position => 4, :col_position => 5)
+    assert king.legal_move?(4, 4)
+    assert king.legal_move?(4, 6)
   end
 
 end
