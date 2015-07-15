096c8771c6ca2246226eba4ffe2ac24a899a02ea
MelanieJae <mtaurus575@gmail.com>
2015-07-15 13:19:08 +0000

    branch cleanup-legal move king

--- app/models/king.rb
+++ app/models/king.rb
@@ -13,7 +13,6 @@ class King < Piece
     return true if  (self.row_position - x) == 0 and (self.col_position - y) == -1
       # move one step diagonally
     return true if  (self.row_position - x).abs == 1 and (self.col_position - y).abs == 1
-    return true if  (self.row_position - x) == -1 and (self.col_position - y) == -1  
   end
 
 end
