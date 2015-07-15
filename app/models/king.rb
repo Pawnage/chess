37495dea02b82bb0b61b9aed9bf953fe2abd6ea9
MelanieJae <mtaurus575@gmail.com>
2015-07-15 12:46:12 +0000

    branch cleanup-legal move king

--- app/models/king.rb
+++ app/models/king.rb
@@ -6,11 +6,11 @@ class King < Piece
 
   def legal_move?(x, y)
       # move one step vertically
-      (self.row_position - x).abs == 1 and (self.col_position - y).abs == 0
+      (self.row_position - x) == 1 and (self.col_position - y) == 0
       # move one step horizontally
-      (self.row_position - x).abs == 0 and (self.col_position - y).abs == 1
+      (self.row_position - x) == 0 and (self.col_position - y) == 1
       # move one step diagonally
-      (self.row_position - x).abs == 1 and (self.col_position - y).abs == 1
+      (self.row_position - x) == 1 and (self.col_position - y) == 1
   
   end
 
