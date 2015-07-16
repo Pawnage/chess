ba3b0e3974680943a7334ee6c0b43be48257f68f
MelanieJae <mtaurus575@gmail.com>
2015-07-16 14:58:27 +0000

    edits to king logic to fix errors

--- app/models/king.rb
+++ app/models/king.rb
@@ -5,6 +5,7 @@ class King < Piece
   end
 
   def legal_move?(x, y)
+    (x >= 0 and x <= 7) and (y >= 0 and y <= 7)
     self.legal_horiz_move?(x, y) || self.legal_vert_move?(x, y) || self.legal_diag_move?(x, y)
   end
 
