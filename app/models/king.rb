5919873f9f20c04a3e8dd5a9846bee5c13fed055
MelanieJae <mtaurus575@gmail.com>
2015-07-16 14:34:30 +0000

    edits to king legal_move to incorporate new basci horiz, vert and diag moves in piece.rb

--- app/models/king.rb
+++ app/models/king.rb
@@ -5,14 +5,7 @@ class King < Piece
   end
 
   def legal_move?(x, y)
-      # move one step horizontally
-    return true if (self.row_position - x) == 1 and (self.col_position - y) == 0
-    return true if (self.row_position - x) == -1 and (self.col_position - y) == 0
-      # move one step vertically
-    return true if (self.row_position - x) == 0 and (self.col_position - y) == 1
-    return true if (self.row_position - x) == 0 and (self.col_position - y) == -1
-      # move one step diagonally
-    return true if (self.row_position - x).abs == 1 and (self.col_position - y).abs == 1
+    self.horiz_move? || self.vert_move? || self.diag_move?
   end
 
 end
