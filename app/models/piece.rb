8dfe97e0b03cb44924f799906e54b525ce1f7f01
MelanieJae <mtaurus575@gmail.com>
2015-07-16 19:03:16 +0000

    edits to model and controller to incorporate xdiff and ydiff calculator methods for use in move logic

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -24,25 +24,36 @@ class Piece < ActiveRecord::Base
 	end
 
   def valid_move?(x, y)
-    self.legal_move?
-    self.obstructed_move?
+    self.attempt_move?(x, y)
   end
 
   def attempt_move?(x, y)
     (x >= 0 and x <= 7) and (y >= 0 and y <= 7)
-      self.valid_move? 
+       #add nil move call?
+      self.legal_move?(x, y)
+      self.obstructed_move?(x, y)
   end
 
-  def legal_horiz_move?(x, y)
-    (self.col_position - y) == 0
+  def x_diff(x)
+    xdif = self.row_position - x
+    xdif
   end
 
-  def legal_vert_move?(x, y)
-    (self.row_position - x) == 0
+  def y_diff(y)
+    ydif = self.col_position - y
+    ydif
+  end
+
+  def legal_horiz_move?(xdiff, ydiff)
+    ydiff == 0
+  end
+
+  def legal_vert_move?(xdiff, ydiff)
+    xdiff == 0
   end  
 
-  def legal_diag_move(x, y)
-    (self.row_position - x) == (self.col_position - y)
+  def legal_diag_move(xdiff, ydiff)
+    xdiff.abs == ydiff.abs
   end
 
   def capturable?(x, y)
