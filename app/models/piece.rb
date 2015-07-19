02160e5740c6edb77b98b3af54685fde064bca1f
MelanieJae <mtaurus575@gmail.com>
2015-07-19 18:18:44 +0000

    edits to piece.rb to add b'dy conds to basic piece moves

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -24,33 +24,21 @@ class Piece < ActiveRecord::Base
 	end
   
   def legal_horiz_move?(x, y)
-    (self.col_position - y) == 0
+    x >= 0 and x <= 7 and y >= 0 and y <= 7 and (self.col_position - y) == 0
   end
 
   def legal_vert_move?(x, y)
-    (self.row_position - x) == 0
+    x >= 0 and x <= 7 and y >= 0 and y <= 7 and (self.row_position - x) == 0
   end  
 
   def legal_diag_move?(x, y)
+    #x >= 0 and x <= 7 and y >= 0 and y <= 7
     (self.row_position - x).abs == (self.col_position - y).abs
   end
 
   def nil_move?(x, y)
     row_position == x && col_position == y
   end
-
-  def legal_horiz_move?(x, y)
-    (self.col_position - y) == 0
-  end
-
-  def legal_vert_move?(x, y)
-    (self.row_position - x) == 0
-  end  
-
-  def legal_diag_move?(x, y)
-    (self.row_position - x).abs == (self.col_position - y).abs
-  end
-
 
   def capturable?(x, y)
     Piece.where(:game_id => game.id, :row_position => x, :col_position => y, :alive => true).where.not(color: color).present?
--- app/models/rook.rb
+++ app/models/rook.rb
@@ -9,12 +9,7 @@ class Rook < Piece
 
   def legal_move?(x, y)
 		#rook's move is purely horizontal or vertical
-		x > 0 and y > 0
-    if self.legal_horiz_move?(x, y) || self.legal_vert_move?(x, y)
-		 		#add conditions for kingside and queenside castling (i.e. rook moves horizontally (or vertically?) around king or queen) 
-      return true
-    else
-      return false	   
-    end
+    self.legal_horiz_move?(x, y) || self.legal_vert_move?(x, y)
+		 		#add conditions for kingside and queenside castling (i.e. rook moves horizontally (or vertically?) around king or queen)
   end
 end
