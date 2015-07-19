d9a7fa6c5bf47f3262f6635fb14d6a8969e8f482
MelanieJae <mtaurus575@gmail.com>
2015-07-19 16:46:38 +0000

    edits to rook logic and tests

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -9,7 +9,7 @@ class Rook < Piece
 
   def legal_move?(x, y)
 		#rook's move is purely horizontal or vertical
-		x >= 0 and x <= 7 and y >= 0 and y <= 7
+		x > 0 and y > 0
     if self.legal_horiz_move?(x, y) || self.legal_vert_move?(x, y)
 		 		#add conditions for kingside and queenside castling (i.e. rook moves horizontally (or vertically?) around king or queen) 
       return true
