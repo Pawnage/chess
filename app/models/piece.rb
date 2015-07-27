21f354a9819c7505117d40cc73fd830e9a897a1c
MelanieJae <mtaurus575@gmail.com>
2015-07-27 21:05:34 +0000

    removed duplicates in piece.rb

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -33,27 +33,11 @@ class Piece < ActiveRecord::Base
     self.attempt_move?(x, y)
   end
 
-  def attempt_move?(x, y)
+  def attempt_move!(x, y)
     #(x >= 0 and x <= 7) and (y >= 0 and y <= 7)
     self.nil_move?(x, y) 
     self.legal_move?(x, y)
     self.obstructed_move?(x, y)
-  end
-
-  def nil_move?(x, y)
-    row_position == x && col_position == y
-  end
-
-  def legal_horiz_move?(x, y)
-    x >= 0 and x <= 7 and y >= 0 and y <= 7 and (self.col_position - y) == 0
-  end
-
-  def legal_vert_move?(x, y)
-    x >= 0 and x <= 7 and y >= 0 and y <= 7 and (self.row_position - x) == 0
-  end  
-
-  def legal_diag_move?(x, y)
-    x >= 0 and x <= 7 and y >= 0 and y <= 7 and (self.row_position - x).abs == (self.col_position - y).abs
   end
 
   def capturable?(x, y)
